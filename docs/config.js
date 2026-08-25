abd7b6726d51c0e1ad0f44987bef7ee34c0b35c0
kitano-sette <naka_bird@hotmail.com>
2026-08-25 14:20:24 +0700

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -5,5 +5,5 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://arjzgujwfsmiheubstqe.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyanpndWp3ZnNtaWhldWJzdHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1OTU0ODIsImV4cCI6MjEwMzE3MTQ4Mn0.bnNX3reVjEf6PfBQlbYVq3mDbdIST4xTxLkLzQc34eY",
-  TRIGGER_FUNCTION_URL: "https://YOUR-PROJECT-REF.supabase.co/functions/v1/trigger-update",
+  TRIGGER_FUNCTION_URL: "https://arjzgujwfsmiheubstqe.supabase.co/functions/v1/trigger-update",
 };
